80f09a441d5aaa3165f524f93e3bc77d98dac6bf
hansu88 <gimhans0917@gmail.com>
2025-11-03 20:50:17 +0900

    "chore: Controller Swagger관련 코드 수정(Product)"

---
+++ src/main/java/com/hhplus/ecommerce/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.hhplus.ecommerce.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi productApi() {
+        return GroupedOpenApi.builder()
+                .group("products")
+                .pathsToMatch("/api/products/**")
+                .build();
+    }
+}
