8eb90f1e5c3d6229a80207b30fd293346a0b1f3a
hansu88 <gimhans0917@gmail.com>
2025-11-04 11:36:26 +0900

    "refactor: Domain Entity 정리(재고관련 비즈니스 로직포함)"

--- src/main/java/com/hhplus/ecommerce/domain/product/Product.java
+++ src/main/java/com/hhplus/ecommerce/domain/product/Product.java
@@ -3,13 +3,16 @@ package com.hhplus.ecommerce.domain.product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * 상품 Entity
  */
 @Getter
+@Setter
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class Product {
@@ -19,7 +22,12 @@ public class Product {
     private ProductStatus status;
     private LocalDateTime createdAt;
 
-    public void setId(long andIncrement) {
-        this.id = andIncrement;
+    private List<ProductOption> options;
+
+    public Product(String name, Integer price, ProductStatus status) {
+        this.name = name;
+        this.price = price;
+        this.status = status;
+        this.createdAt = LocalDateTime.now();
     }
 }
--- src/main/java/com/hhplus/ecommerce/domain/product/ProductOption.java
+++ src/main/java/com/hhplus/ecommerce/domain/product/ProductOption.java
@@ -1,13 +1,17 @@
 package com.hhplus.ecommerce.domain.product;
 
+import com.hhplus.ecommerce.domain.stock.StockHistory;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import java.util.List;
 
 /**
  * 상품 옵션 Entity
  */
 @Getter
+@Setter
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class ProductOption {
@@ -17,7 +21,25 @@ public class ProductOption {
     private String size;
     private Integer stock;
 
-    public void setId(long andIncrement) {
+    private List<StockHistory> stockHistories;
 
+    public ProductOption(Long productId, String color, String size, Integer stock) {
+        this.productId = productId;
+        this.color = color;
+        this.size = size;
+        this.stock = stock;
+    }
+
+    // 재고 차감
+    public void reduceStock(int quantity) {
+        if (stock < quantity) {
+            throw new IllegalStateException("재고 부족: 현재 재고 " + stock);
+        }
+        stock -= quantity;
+    }
+
+    // 재고 복원
+    public void restoreStock(int quantity) {
+        stock += quantity;
     }
 }
