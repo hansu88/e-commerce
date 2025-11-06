7b39022e4f07083434310d00362be80a07c80a56
hansu88 <gimhans0917@gmail.com>
2025-11-07 04:20:50 +0900

    "chore: 쿠폰Entity 수정"

--- src/main/java/com/hhplus/ecommerce/domain/coupon/Coupon.java
+++ src/main/java/com/hhplus/ecommerce/domain/coupon/Coupon.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * 쿠폰 Entity
@@ -16,11 +16,11 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Coupon {
     private Long id;
-    private String name;                // 쿠폰 이름
     private String code;
-    private Integer discountAmount;     // 할인 금액 (고정 금액)
+    private Integer discountAmount;
     private Integer totalQuantity;      // 총 발급 가능 수량
     private Integer issuedQuantity;     // 현재까지 발급된 수량
-    private LocalDate validFrom;
-    private LocalDate validUntil;
+    private LocalDateTime validFrom;
+    private LocalDateTime validUntil;
+
 }
