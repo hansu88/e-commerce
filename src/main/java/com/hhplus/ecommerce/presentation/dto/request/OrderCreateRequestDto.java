c2e465f14bd592cad97e0079545efc397b308f53
hansu88 <gimhans0917@gmail.com>
2025-11-06 23:42:10 +0900

    "chore: 주문 응답 관련 dto 수정"

--- src/main/java/com/hhplus/ecommerce/presentation/dto/request/OrderCreateRequestDto.java
+++ src/main/java/com/hhplus/ecommerce/presentation/dto/request/OrderCreateRequestDto.java
@@ -2,10 +2,13 @@ package com.hhplus.ecommerce.presentation.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class OrderCreateRequestDto {
     private Long userId;
     private List<CartItemInfo> cartItems;
@@ -13,8 +16,10 @@ public class OrderCreateRequestDto {
 
     @Getter
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class CartItemInfo {
         private Long cartItemId;
+        private Long productOptionId;  // 추가
         private Integer quantity;
     }
 }
