c878a7e77d3e31fd163df78e3a0816ca546a289d
hansu88 <gimhans0917@gmail.com>
2025-11-06 20:20:53 +0900

    "feat: swagger 기능"

--- src/main/java/com/hhplus/ecommerce/config/SwaggerConfig.java
+++ src/main/java/com/hhplus/ecommerce/config/SwaggerConfig.java
@@ -8,10 +8,42 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
+    public GroupedOpenApi allApi() {
+        return GroupedOpenApi.builder()
+                .group("all")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
     public GroupedOpenApi productApi() {
         return GroupedOpenApi.builder()
                 .group("products")
                 .pathsToMatch("/api/products/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi cartApi() {
+        return GroupedOpenApi.builder()
+                .group("carts")
+                .pathsToMatch("/api/carts/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi couponApi() {
+        return GroupedOpenApi.builder()
+                .group("coupons")
+                .pathsToMatch("/api/coupons/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi orderApi() {
+        return GroupedOpenApi.builder()
+                .group("orders")
+                .pathsToMatch("/api/orders/**")
                 .build();
     }
 }
