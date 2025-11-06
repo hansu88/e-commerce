9f3612e2b1d40aaa4c5993f02ec42efabab286ae
hansu88 <gimhans0917@gmail.com>
2025-11-07 04:21:55 +0900

    "chore: 주문 Entity 수정"

--- src/main/java/com/hhplus/ecommerce/domain/order/OrderItem.java
+++ src/main/java/com/hhplus/ecommerce/domain/order/OrderItem.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 
 /**
  * 주문 항목 엔티티
+ * 인기 상품 집계를 위해 createdAt 필드 포함
  */
 @Getter
 @Setter
@@ -21,4 +22,15 @@ public class OrderItem {
     private Integer quantity;
     private Integer price;
     private LocalDateTime createdAt;
+    
+
+    /**
+     * 인기 상품 집계를 위한 헬퍼 메서드
+     * ProductOption의 productId를 얻기 위해 사용
+     */
+    public Long getProductId() {
+        // TODO: ProductOption에서 productId 조회 필요
+        // 현재는 별도 메서드로 처리
+        return null;
+    }
 }
